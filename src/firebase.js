65dbc38bc255ef25d54666d1c60ce0c227a44b2d
Dhanesh-GL <dhaneshgl024@gmail.com>
2024-07-10 19:42:12 +0530

    Adding files

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBx3ISmrXvU0l5iPKdWPpJt9krE0E7pffU",
-  authDomain: "blog-35c2f.firebaseapp.com",
-  projectId: "blog-35c2f",
-  storageBucket: "blog-35c2f.appspot.com",
-  messagingSenderId: "424954291009",
-  appId: "1:424954291009:web:319dff2e8e4f1962fbb7c0",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
