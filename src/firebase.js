1e76950d61c1566b41dc04fd66e7aa1922952425
Dhanesh-GL <dhaneshgl024@gmail.com>
2024-07-10 18:13:30 +0530

    1

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBx3ISmrXvU0l5iPKdWPpJt9krE0E7pffU",
+  authDomain: "blog-35c2f.firebaseapp.com",
+  projectId: "blog-35c2f",
+  storageBucket: "blog-35c2f.appspot.com",
+  messagingSenderId: "424954291009",
+  appId: "1:424954291009:web:319dff2e8e4f1962fbb7c0",
+};
+
+export const app = initializeApp(firebaseConfig);
